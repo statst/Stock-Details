e292ab41c7def8b612427dbcc23fe351432d29ef
Jyoti Shinde <jyoti@Jyotis-MacBook-Pro.local>
2022-01-09 23:11:03 +0530

    displaying more stocks

--- src/app/components/stock-records1/stock-records1.component.ts
+++ src/app/components/stock-records1/stock-records1.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IStockDetail } from 'src/app/models/stocks.model';
+import { StockService } from 'src/app/services/stock.service';
 
 @Component({
   selector: 'app-stock-records1',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-records1.component.css']
 })
 export class StockRecords1Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public stockRecords: IStockDetail[];
+  constructor(private stockService: StockService) { 
+    this.stockRecords = [];
   }
 
+  public ngOnInit(): void {
+    this.initializeStockRecords();
+  }
+
+  public initializeStockRecords():void{
+    this.stockRecords = this.stockService.getStockDetails().splice(0,2)
+  }
 }
