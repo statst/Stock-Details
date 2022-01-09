466425b00224733bca864b4a5a43c0301cd794fc
Jyoti Shinde <jyoti@Jyotis-MacBook-Pro.local>
2022-01-09 23:02:00 +0530

    updated stock service

--- src/app/components/stocks/stocks.component.ts
+++ src/app/components/stocks/stocks.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IStockDetail } from 'src/app/models/stocks.model';
+import { StockService } from 'src/app/services/stock.service';
 
 @Component({
   selector: 'app-stocks',
@@ -16,7 +17,7 @@ export class StocksComponent implements OnInit {
 
   public isEditStockButtonClicked : Boolean;
   public isEditStockButtonClickedDetails : Boolean[];
-  constructor() {
+  constructor(private stockService: StockService) {
 
     // variable initialization at the constructor level
       this.stockInfo = '';
@@ -49,42 +50,7 @@ export class StocksComponent implements OnInit {
       symbol: "BIOS"
     }
     // Array list of objects
-    this.stockDetails = [{
-      id: 1,
-      stockName: "BioScrip, Inc.",
-      stockMarketCap: 306.888888888888809,
-      stockMSector: "Health Care",
-      releaseDate: "01/06/2021",
-      symbol: "BIOS"
-    }, {
-      id: 2,
-      stockName: "RiceBran Technologies",
-      stockMarketCap: 9.62,
-      stockMSector: "Consumer Non-Durables",
-      releaseDate: "11/05/2021",
-      symbol: "RIBT"
-    }, {
-      id: 3,
-      stockName: "John Hancock Investors Trust",
-      stockMarketCap: 152.46,
-      stockMSector: "Health Care",
-      releaseDate: "12/05/2021",
-      symbol: "JHI"
-    }, {
-      id: 4,
-      stockName: "Yatra Online, Inc.",
-      stockMarketCap: 398.74,
-      stockMSector: "Consumer Services",
-      releaseDate: "10/03/2021",
-      symbol: "YTRA"
-    }, {
-      id: 5,
-      stockName: "Restaurant Brands International Inc.",
-      stockMarketCap: 14.34,
-      stockMSector: "Consumer Services",
-      releaseDate: "06/07/2021",
-      symbol: "QSR"
-    }]
+    this.stockDetails = this.stockService.getStockDetails();
 
     this.fillEditButtonArray();
   }
